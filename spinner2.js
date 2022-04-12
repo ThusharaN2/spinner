df4f5676cc00d917cc4832714331e69f2f732471
Thushara <thusharan@hotmail.com>
2022-04-12 19:45:33 -0400

    added \n

--- spinner2.js
+++ spinner2.js
@@ -3,7 +3,7 @@ process.stdout.write('hello from spinner2.js... \rheyyy\n');
 
 const spinner2 = function() {
   let delay = 100;
-  let spinCycle = ["\r|   ", "\r/   ", "\r-   ", "\r\\   ", "\r|   "];
+  let spinCycle = ["\r|   ", "\r/   ", "\r-   ", "\r\\   ", "\r|   ", "\n"];
 
   for (const spin of spinCycle) {
     setTimeout(() => {
